cfe511f7e3f6d950e10e6d96f86988eb0bf5d388
BoMeneerNL <51164986+BoMeneerNL@users.noreply.github.com>
2021-08-28 20:38:38 +0200

    update

--- scripts/js/jeffss-main.js
+++ scripts/js/jeffss-main.js
@@ -4,15 +4,10 @@ function login() {
     let password = document.getElementById("password").value;
 
     //Check Username & Password
-    if (username == "" || password == "") {
-        alert("oops, no data found (Error_Recieved_NULL_Expected_*++)")
-    } else if (username === "Jeff" && password === "JeffsPassword") {
-        loginshow()
-    } else if (username === "bigchungus" && password === "bigchungus") {
-        loginshow()
-    } else {
-        alert("Oops, your credentials doesn't match with the system knows credentials (err_JJSF_CRNC_0AUTH)")
-    }
+    if (username == "" || password == "") alert("oops, no data found (Error_Recieved_NULL_Expected_*++)")
+	else if (username === "Jeff" && password === "JeffsPassword")loginshow()
+	else if (username === "bigchungus" && password === "bigchungus") loginshow()
+	else alert("Oops, your credentials doesn't match with the system knows credentials (err_JJSF_CRNC_0AUTH)")
 
 }
 
@@ -25,7 +20,7 @@ function loginshow() {
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
     alert("Welcome back " + username + ", you have been logged in")
-    document.getElementById("welcome").innerHTML = "Welcome " + username + " to this page, do you want to do an action?";
+    document.getElementById("welcome").innerHTML = `Welcome \"${username} + \" to this page, do you want to do an action?`;
     loginbuilder()
 }
 
--- scripts/js/min.jeffss-emailcontent.js
+++
@@ -1,6 +0,0 @@
-function getfunction(){
-    
-}
-function emc1(){
-
-}
