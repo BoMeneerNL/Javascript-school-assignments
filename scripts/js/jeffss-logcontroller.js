0ae0d7f92e2cfc3bc1917af8f1891e7cd19d795a
BoMeneerNL <51164986+BoMeneerNL@users.noreply.github.com>
2022-11-20 02:50:37 +0100

    code update

--- scripts/js/jeffss-logcontroller.js
+++ scripts/js/jeffss-logcontroller.js
@@ -7,18 +7,18 @@ function getcurrentlog(action) {
 }
 
 function logger(jeff, action) {
-    if (jeff == 0) {
+    if (jeff === 0) {
         console.log("Oops something went wrong (err_noup_logcontroller)");
     } else {
-        logc = "logc" + logcount
-        var today = new Date();
-        var h = today.getHours();
-        var m = today.getMinutes();
-        var s = today.getSeconds();
-        var d = today.getDate();
-        var mo = today.getMonth() + 1;
-        var y = today.getUTCFullYear();
-        timestamp = " ( " + h + ":" + m + ":" + s + " @ " + d + "-" + mo + "-" + y + " )"
+        const logc = "logc" + logcount
+        const today = new Date();
+        const h = today.getHours();
+        const m = today.getMinutes();
+        const s = today.getSeconds();
+        const d = today.getDate();
+        const mo = today.getMonth() + 1;
+        const y = today.getUTCFullYear();
+        const timestamp = `(${h}:${m}:${s} @ ${d}-${mo}-${y})`
         document.getElementById(logc).style.display = "initial"
         document.getElementById(logc).innerHTML = logcount + ". " + action + timestamp
 
--- scripts/js/jeffss-main.js
+++ scripts/js/jeffss-main.js
@@ -4,17 +4,12 @@ function login() {
     let password = document.getElementById("password").value;
 
     //Check Username & Password
-    if (username == "" || password == "") alert("oops, no data found (Error_Recieved_NULL_Expected_*++)")
+    if (username === "" || password === "") alert("oops, no data found (Error_Recieved_NULL_Expected_*++)")
 	else if (username === "Jeff" && password === "JeffsPassword")loginshow()
 	else if (username === "bigchungus" && password === "bigchungus") loginshow()
 	else alert("Oops, your credentials doesn't match with the system knows credentials (err_JJSF_CRNC_0AUTH)")
 
 }
-
-function logout() {
-    window.location.reload();
-}
-
 //Welcome Screen
 function loginshow() {
     let username = document.getElementById("username").value;
@@ -37,25 +32,4 @@ function loginbuilder() {
 
     //Change title
     document.getElementById("title").innerHTML = "Welcome to this page. an action?, or leaving?"
-}
-
-//Rebuild1
-function rebuild1() {
-    document.getElementById("")
-    document.getElementById("message.1").style.display = "initial"
-    document.getElementById("btn1.class1").style.display = "initial"
-    document.getElementById("btn2.class1").style.display = "initial"
-    document.getElementById("btn3.class1").style.display = "initial"
-    document.getElementById("yesb1").style.display = "none"
-    document.getElementById("nob1").innerHTML = "Logout"
-    //Change title
-    document.getElementById("title").innerHTML = "Please, choose your action"
-}
-//shows empty email
-function email() {
-    document.getElementById("message.1").style.display = "none"
-    document.getElementById("btn1.class1").style.display = "none"
-    document.getElementById("btn2.class1").style.display = "none"
-    document.getElementById("btn3.class1").style.display = "none"
-    document.getElementById("email.empty").style.display = "initial"
 }
